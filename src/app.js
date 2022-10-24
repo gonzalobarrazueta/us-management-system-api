8bb4277a62b807e216cd2f9dcad4dba4f7506e81
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2022-10-24 13:33:28 +0200

    added cors package

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from 'express'
+import cors from 'cors';
 
 import usersRoutes from './routes/users.routes.js'
 import indexRoutes from './routes/index.routes.js'
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json(), cors());
 
 app.use(indexRoutes);
 app.use('/api', usersRoutes);
